9d0d2f222cbf561c82c7f1f77243117d6c95684d
tdpaul315@gmail.com <tiaunaj.tsg@gmail.com>
2020-12-04 23:27:09 -0500

    commit tree d37b450398bdba4b5ca3f8a3247d5090fed56bcb

--- main.js
+++ main.js
@@ -4,6 +4,36 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const modal = document.querySelector('#modal');
+const message = document.querySelector('#modal-message');
+
+
+let hearts = document.querySelectorAll(".like")
+
+
+for (let heart of hearts) {
+  heart.addEventListener("click", like)
+}
+
+function like(e) {
+  if (e.target.innerText === EMPTY_HEART) {
+    mimicServerCall()
+    .then(() => {
+      e.target.innerText = FULL_HEART;
+      e.target.classList.toggle('activated-heart');
+    })
+    .catch(err => {
+      modal.classList.toggle('hidden');
+      message.innerText = err;
+      setTimeout(() => modal.classList.toggle('hidden'), 5000);
+    })
+  }
+  if (e.target.innerText === FULL_HEART) {
+    e.target.innerText = EMPTY_HEART;
+    e.target.classList.toggle('activated-heart');
+  }
+}
+
 
 
 
